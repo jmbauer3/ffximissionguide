81260f820e9f1e7575f7085bbd6b7e9db357df1e
John M. Bauer <161013370+jmbauer3@users.noreply.github.com>
2024-12-07 01:46:22 +0100

    Update MissionGuide.lua
    still broken but starting to work

--- MissionGuide.lua
+++ MissionGuide.lua
@@ -1,61 +1,94 @@
-_addon.name = 'MissionGuide'
-_addon.author = 'YourName'
-_addon.version = '1.0'
-_addon.commands = {'mission'}
+addon.name      = 'MissionGuide';
+addon.author    = 'YourName';
+addon.version   = '1.3';
+addon.desc      = 'Guides players through mission objectives.';
+addon.link      = 'https://ashitaxi.com/';
 
--- Mission data
-local missions = {
-    ["Zilart Missions"] = {
-        { mission = "ZM1: The New Frontier", location = "Norg", note = "Speak to Gilgamesh to start the mission." },
-        { mission = "ZM2: Welcome t' Norg", location = "Norg", note = "Speak to Ryoma in Norg to complete." },
-        { mission = "ZM3: Kazham's Chieftainess", location = "Kazham", note = "Speak to Romaa Mihgo in Kazham." },
-        { mission = "ZM4: The Temple of Uggalepih", location = "Temple of Uggalepih", note = "Examine the Paintbrush Door for a cutscene." },
-        { mission = "ZM5: Headstone Pilgrimage", location = "Various Locations", note = "Examine headstones at Cloister zones. Prepare for fights." },
-        { mission = "ZM6: Through the Quicksand Caves", location = "Quicksand Caves", note = "Defeat the Antican NMs and examine the door." },
-        { mission = "ZM7: The Chamber of Oracles", location = "Ru'Aun Gardens", note = "Examine the Celestial Nexus for a cutscene." },
-        { mission = "ZM8: Return to Delkfutt's Tower", location = "Delkfutt's Tower", note = "Defeat the NM and proceed to the top for a cutscene." },
-        { mission = "ZM9: Ro'Maeve", location = "Ro'Maeve", note = "Obtain the Lightbringer Key Item." },
-        { mission = "ZM10: The Celestial Nexus", location = "Ru'Aun Gardens", note = "Prepare for a difficult fight at the Celestial Nexus." },
-        { mission = "ZM11: The Hall of the Gods", location = "Hall of the Gods", note = "Return to the Hall of the Gods to complete." },
-        { mission = "ZM12: The Mithra and the Crystal", location = "Various Locations", note = "Follow up at Norg and Ru'Aun Gardens." },
-        { mission = "ZM13: Ark Angels", location = "Various Locations", note = "Defeat the five Ark Angels in separate battles." },
-        { mission = "ZM14: Awakening", location = "Celestial Nexus", note = "Final battle against Kam'lanaut. Cutscenes will follow." }
-    }
-}
+require('common');
 
--- Current mission index (default to the first mission)
-local current_mission_index = 1
-local current_category = "Zilart Missions"
+-- MissionGuide Variables
+local missionguide = {
+    current_mission = 1, -- Start at the first mission
+    missions = {
+        { name = "The New Frontier", location = "Ru'Lude Gardens", description = "Speak to Monberaux to begin your journey." },
+        { name = "Through the Quicksand Caves", location = "Western Altepa Desert", description = "Find and use the Quicksand Caves entrance." },
+        { name = "Return to Delkfutt's Tower", location = "Delkfutt's Tower", description = "Retrieve the key item at the top." },
+    },
+};
 
--- Command handling
-ashita.register_event('command', function(command, nType)
-    local args = command:args()
+-- Utility function to split a string into parts
+local function split(input, delimiter)
+    local result = {};
+    for match in (input .. delimiter):gmatch("(.-)" .. delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
 
-    if args[1] == '/mission' then
+--[[
+* event: load
+* desc : Event called when the addon is being loaded.
+--]]
+ashita.events.register('load', 'load_cb', function()
+    -- Debug: Print current state upon load
+    print('[MissionGuide] Addon loaded successfully!');
+    print('[MissionGuide] Use /mg current to check your current mission or /mg next to begin!');
+end);
+
+--[[
+* event: unload
+* desc : Event called when the addon is being unloaded.
+--]]
+ashita.events.register('unload', 'unload_cb', function()
+    print('[MissionGuide] Addon unloaded.');
+end);
+
+--[[
+* event: command
+* desc : Event called when a command is entered.
+--]]
+ashita.events.register('command', 'command_cb', function(command, nType)
+    local cmd = tostring(command):lower(); -- Convert command userdata to string and lowercase it
+    local args = split(cmd, ' ');
+    
+    if args[1] == '/mg' then
         if args[2] == 'next' then
-            if missions[current_category][current_mission_index] then
-                local mission = missions[current_category][current_mission_index]
-                print(string.format("Next Mission: %s", mission.mission))
-                print(string.format("Location: %s", mission.location))
-                print(string.format("Note: %s", mission.note))
+            -- Display next mission
+            local mission = missionguide.missions[missionguide.current_mission];
+            if mission then
+                print(string.format('[MissionGuide] Next Mission:\nName: %s\nLocation: %s\nDescription: %s',
+                    mission.name, mission.location, mission.description
+                ));
             else
-                print("No more missions in this category!")
+                print('[MissionGuide] No more missions available.');
             end
-            return true
+            return true;
         elseif args[2] == 'complete' then
-            current_mission_index = current_mission_index + 1
-            print("Mission marked as complete. Use '/mission next' for the next step.")
-            return true
-        elseif args[2] == 'set' and args[3] then
-            current_mission_index = tonumber(args[3])
-            print("Mission index set to: " .. current_mission_index)
-            return true
-        elseif args[2] == 'category' and args[3] then
-            current_category = args[3]
-            current_mission_index = 1
-            print("Category set to: " .. current_category)
-            return true
+            -- Mark mission as complete
+            if missionguide.current_mission < #missionguide.missions then
+                missionguide.current_mission = missionguide.current_mission + 1;
+                print('[MissionGuide] Mission marked as complete. Use /mg next to see the next mission.');
+            else
+                print('[MissionGuide] All missions completed!');
+            end
+            return true;
+        elseif args[2] == 'current' then
+            -- Display current mission
+            local mission = missionguide.missions[missionguide.current_mission];
+            if mission then
+                print(string.format('[MissionGuide] Current Mission:\nName: %s\nLocation: %s\nDescription: %s',
+                    mission.name, mission.location, mission.description
+                ));
+            else
+                print('[MissionGuide] No active mission. Use /mg next to start!');
+            end
+            return true;
+        else
+            -- Handle invalid or missing commands
+            print('[MissionGuide] Invalid command. Usage: /mg [next|complete|current]');
+            return true;
         end
     end
-    return false
-end)
+    
+    return false;
+end);
